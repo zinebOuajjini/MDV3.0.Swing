4a21de6b86942860a8e163d0ec077de779d4c81c
OUAJJINI Zineb <zineb.ouajjini@soprabanking.com>
2020-04-01 09:54:06 +0100

    first commit

---
+++ src/main/java/com/sopra/banking/AukApplication.java
@@ -0,0 +1,66 @@
+package com.sopra.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AukApplication extends JFrame {
+	
+	
+	//const
+	public AukApplication() {
+		
+		initUI();
+		
+	}
+	
+	private void initUI() {
+
+	    var quitButton = new JButton("Quit");
+
+	    quitButton.addActionListener((ActionEvent event) -> {
+	        System.exit(0);
+	    });
+
+	    createLayout(quitButton);
+
+	    setTitle("Quit button");
+	    setSize(300, 200);
+	    setLocationRelativeTo(null);
+	    setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private void createLayout(JComponent... arg) {
+
+	    var pane = getContentPane();
+	    var gl = new GroupLayout(pane);
+	    pane.setLayout(gl);
+
+	    gl.setAutoCreateContainerGaps(true);
+
+	    gl.setHorizontalGroup(gl.createSequentialGroup()
+	            .addComponent(arg[0])
+	    );
+
+	    gl.setVerticalGroup(gl.createSequentialGroup()
+	            .addComponent(arg[0])
+	    );
+	}
+	
+	public static void main(String[] args) {
+		
+		 var ctx = new SpringApplicationBuilder(AukApplication.class)
+		            .headless(false).run(args);
+
+		    EventQueue.invokeLater(() -> {
+
+		        var ex = ctx.getBean(AukApplication.class);
+		        ex.setVisible(true);
+		    });
+	}
+
+}
+
+
+
+
---
+++ src/test/java/com/sopra/banking/AukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopra.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
